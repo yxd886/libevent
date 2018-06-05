37de77b69db083ff9f5809a397ff0b3c9ccdcd82
xiaodongyi <409070169@qq.com>
2018-06-05 17:47:11 +0800

    aa

--- http.c
+++ http.c
@@ -1302,6 +1302,7 @@ evhttp_request_dispatch(struct evhttp_connection* evcon)
 	/* Create the header from the store arguments */
 	evhttp_make_header(evcon, req);
 	char content[4096];
+	memset(content,0,4096);
 	struct evbuffer* output_buffer = bufferevent_get_output(evhttp_connection_get_bufferevent(evcon));
    // output_buffer = bufferevent_get_output(evhttp_connection_get_bufferevent(evcon));
     size_t _len = evbuffer_get_length(output_buffer);
