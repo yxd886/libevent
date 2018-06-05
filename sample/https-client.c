46cea186cc2c4d3d35009ba6dff5f8b32345d8e0
xiaodongyi <409070169@qq.com>
2018-06-05 17:34:59 +0800

    aa

--- sample/https-client.c
+++ sample/https-client.c
@@ -73,7 +73,45 @@ enum { HTTP, HTTPS } type = HTTP;
 static void
 http_request_done(struct evhttp_request *req, void *ctx)
 {
-    printf("make new request\n");
+    char buffer[256];
+    int nread;
+
+    if (req == NULL) {
+        /* If req is NULL, it means an error occurred, but
+         * sadly we are mostly left guessing what the error
+         * might have been.  We'll do our best... */
+        struct bufferevent *bev = (struct bufferevent *) ctx;
+        unsigned long oslerr;
+        int printed_err = 0;
+        int errcode = EVUTIL_SOCKET_ERROR();
+        fprintf(stderr, "some request failed - no idea which one though!\n");
+        /* Print out the OpenSSL error queue that libevent
+         * squirreled away for us, if any. */
+        while ((oslerr = bufferevent_get_openssl_error(bev))) {
+            ERR_error_string_n(oslerr, buffer, sizeof(buffer));
+            fprintf(stderr, "%s\n", buffer);
+            printed_err = 1;
+        }
+        /* If the OpenSSL error queue was empty, maybe it was a
+         * socket error; let's try printing that. */
+        if (! printed_err)
+            fprintf(stderr, "socket error = %s (%d)\n",
+                evutil_socket_error_to_string(errcode),
+                errcode);
+        return;
+    }
+
+    fprintf(stderr, "Response line: %d %s\n",
+        evhttp_request_get_response_code(req),
+        evhttp_request_get_response_code_line(req));
+
+    while ((nread = evbuffer_remove(evhttp_request_get_input_buffer(req),
+            buffer, sizeof(buffer)))
+           > 0) {
+        /* These are just arbitrary chunks of 256 bytes.
+         * They are not lines, so we can't treat them as such. */
+        fwrite(buffer, nread, 1, stdout);
+    }
 
 
 }
