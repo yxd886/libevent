a5c2a0cd3d499033c5bde146e4f28d0afdc46c81
xiaodongyi <409070169@qq.com>
2018-06-05 17:05:25 +0800

    request

--- sample/https-client.c
+++ sample/https-client.c
@@ -44,6 +44,7 @@
 #include <openssl/rand.h>
 
 #include "openssl_hostname_validation.h"
+#include "aes.hh"
 
 
 static struct event_base *base;
@@ -54,43 +55,27 @@ http_request_done(struct evhttp_request *req, void *ctx)
 {
 	char buffer[256];
 	int nread;
+	struct bufferevent *bev = (struct bufferevent *) ctx;
+    char content[4096];
+    struct evbuffer* output_buffer = bufferevent_get_output(bev);
+   // output_buffer = bufferevent_get_output(evhttp_connection_get_bufferevent(evcon));
+    size_t _len = evbuffer_get_length(output_buffer);
+    printf("len:%d\n",_len);
+    evbuffer_remove(output_buffer,content,_len);
+    printf("before decrypt\n");
+    printf("%.*s\n", _len, content);
+    if(_len % AES_BLOCKLEN != 0) {
+        _len = (_len / AES_BLOCKLEN + 1) * AES_BLOCKLEN;
+    }
+    struct AES_ctx _ctx;
+    for(int i = 0; i < AES_KEYLEN; i++){
+        key[i] = i * 2;
+    }
+    AES_init_ctx_iv(&_ctx, key, iv);
+    AES_CBC_decrypt_buffer(&_ctx, content, _len);
+    printf("after decrypt\n");
+    printf("%.*s\n", _len, content);
 
-	if (req == NULL) {
-		/* If req is NULL, it means an error occurred, but
-		 * sadly we are mostly left guessing what the error
-		 * might have been.  We'll do our best... */
-		struct bufferevent *bev = (struct bufferevent *) ctx;
-		unsigned long oslerr;
-		int printed_err = 0;
-		int errcode = EVUTIL_SOCKET_ERROR();
-		fprintf(stderr, "some request failed - no idea which one though!\n");
-		/* Print out the OpenSSL error queue that libevent
-		 * squirreled away for us, if any. */
-		while ((oslerr = bufferevent_get_openssl_error(bev))) {
-			ERR_error_string_n(oslerr, buffer, sizeof(buffer));
-			fprintf(stderr, "%s\n", buffer);
-			printed_err = 1;
-		}
-		/* If the OpenSSL error queue was empty, maybe it was a
-		 * socket error; let's try printing that. */
-		if (! printed_err)
-			fprintf(stderr, "socket error = %s (%d)\n",
-				evutil_socket_error_to_string(errcode),
-				errcode);
-		return;
-	}
-
-	fprintf(stderr, "Response line: %d %s\n",
-	    evhttp_request_get_response_code(req),
-	    evhttp_request_get_response_code_line(req));
-
-	while ((nread = evbuffer_remove(evhttp_request_get_input_buffer(req),
-		    buffer, sizeof(buffer)))
-	       > 0) {
-		/* These are just arbitrary chunks of 256 bytes.
-		 * They are not lines, so we can't treat them as such. */
-		fwrite(buffer, nread, 1, stdout);
-	}
 }
 
 static void
@@ -468,14 +453,13 @@ main(int argc, char **argv)
 		evhttp_add_header(output_headers, "Content-Length", buf);
 		fclose(f);
 	}
-	int k=0;
-	for(k=0;k<100;k++){
-	    r = evhttp_make_request(evcon, req, data_file ? EVHTTP_REQ_POST : EVHTTP_REQ_GET, uri);
-	    if (r != 0) {
-	        fprintf(stderr, "evhttp_make_request() failed\n");
-	        goto error;
-	    }
-	}
+
+    r = evhttp_make_request(evcon, req, data_file ? EVHTTP_REQ_POST : EVHTTP_REQ_GET, uri);
+    if (r != 0) {
+        fprintf(stderr, "evhttp_make_request() failed\n");
+        goto error;
+    }
+
 
 
 	event_base_dispatch(base);
