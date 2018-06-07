d7e1f7a737e8aef742b0d94d66759e01e375c28d
xiaodongyi <409070169@qq.com>
2018-06-07 15:09:41 +0800

    evutil

--- evutil.c
+++ evutil.c
@@ -378,6 +378,7 @@ evutil_make_listen_socket_reuseable_port(evutil_socket_t sock)
 	int one = 1;
 	/* REUSEPORT on Linux 3.9+ means, "Multiple servers (processes or
 	 * threads) can bind to the same port if they each set the option. */
+	printf("reuse\n");
 	return setsockopt(sock, SOL_SOCKET, SO_REUSEPORT, (void*) &one,
 	    (ev_socklen_t)sizeof(one));
 #else
